956567af04387579511fac7356596acbeb59c48e
FedeDinuzzo <68045599+FedeDinuzzo@users.noreply.github.com>
2024-08-24 15:59:28 -0300

    Tape

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,8 +1,32 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Performantes",
+  "Accesibles",
+  "Seguras",
+  "Interactivas",
+  "Escalables",
+  "Faciles de usar",
+  "Mantenibles",
+  "Confiables",
+  "Interactivas",
+];
+
 export const TapeSection = () => {
   return (
-    <section className="">
-      <div className="container">
-        <div></div>
+    <section className="py-16 lg:py-24">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map((word) => (
+              <div key={word} className="inline-flex gap-4 items-center">
+                <span className="text-gray-900 font-extrabold uppercase text-sm">{word}</span>
+                <StarIcon className="size-6 text-gray-900 -rotate-12" />
+              </div>
+            ))}
+          </div>
+          \
+        </div>
       </div>
     </section>
   );
