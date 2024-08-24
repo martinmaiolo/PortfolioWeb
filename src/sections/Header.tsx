28bb0914c159a3ebec581afbaf5776ddd42b3b66
FedeDinuzzo <68045599+FedeDinuzzo@users.noreply.github.com>
2024-08-24 02:18:34 -0300

    Finish Hero

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,18 +1,18 @@
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center sticky mt-3">
+    <div className="flex justify-center items-center fixed z-10 top-3 w-full">
       <nav className="bgBlur flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10">
         <a href="#" className="nav-item">
-          Home
+          Inicio
         </a>
         <a href="#" className="nav-item">
-          Projects
+          Proyectos
         </a>
         <a href="#" className="nav-item">
-          About
+          Sobre mi
         </a>
         <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/70">
-          Contact
+          Contacto
         </a>
       </nav>
     </div>
