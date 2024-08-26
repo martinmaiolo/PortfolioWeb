ec6982c42fdcadd6ace0d302156469518467d622
FedeDinuzzo <68045599+FedeDinuzzo@users.noreply.github.com>
2024-08-26 00:39:59 -0300

    Footer

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -7,7 +7,7 @@ export const ContactSection = () => {
       <div className="container">
         <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
           <div className="absolute inset-0 -z-10 opacity-5" style={{ backgroundImage: `url(${grainImage.src})` }}></div>
-          <div className="flex flex-col items-center md:flex-row gap-8 md:gap-16">
+          <div className="flex flex-col items-center md:flex-row md:gap-16">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">Vamos a crear algo muy increible juntos</h2>
               <p className="text-sm md:text-base mt-2">
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,40 @@
+import ArrowUpRight from "@/assets/icons/arrow-up-right.svg";
+
+const footerLink = [
+  {
+    title: "Youtube",
+    href: "#",
+  },
+  {
+    title: "Twitter",
+    href: "#",
+  },
+  {
+    title: "Telegram",
+    href: "#",
+  },
+  {
+    title: "LinkedIn",
+    href: "#",
+  },
+];
 export const Footer = () => {
   return (
-    <section className="">
+    <footer className="relative -z-10 overflow-x-clip">
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
       <div className="container">
-        <div></div>
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row items-center md:justify-between gap-8">
+          <div className="text-white/50">&copy; 2024. All rights reserved.</div>
+          <nav className="flex flex-col md:flex-row items-center gap-8">
+            {footerLink.map((link) => (
+              <a key={link.title} href={link.href} className="inline-flex items-center gap-1.5">
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRight className="size-4" />
+              </a>
+            ))}
+          </nav>
+        </div>
       </div>
-    </section>
+    </footer>
   );
 };
