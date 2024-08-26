466301aa759cdb84cd91b88504553669f1808fe3
FedeDinuzzo <68045599+FedeDinuzzo@users.noreply.github.com>
2024-08-25 21:03:20 -0300

    Styling About Section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -21,6 +21,7 @@ import barista from "@/assets/images/barista.png";
 import guitarra from "@/assets/images/guitarra.png";
 import viajar from "@/assets/images/viajar.png";
 import rock from "@/assets/images/rock.png";
+import { CardHeader } from "@/components/CardHeader";
 
 const toolboxItems = [
   {
@@ -90,47 +91,53 @@ const hobbies = [
 
 export const AboutSection = () => {
   return (
-    <section className="">
-      <SectionHeader
-        text="Sobre Mi"
-        title="Un Vistazo De Mi Mundo"
-        description="Aprende mas sobre quien soy, lo que hago y lo que me inspira"
-      />
-      <div className="">
-        <Card>
-          <div>
-            <StarIcon />
-            <h2>Mis Logros</h2>
-            <p>Explora las certificaciones que avalan mis conocimientos.</p>
-          </div>
-          <Image src={bookImage} alt="Book Cover" />
-        </Card>
-        <Card>
-          <div>
-            <StarIcon />
-            <h2>Mis Herramientas</h2>
-            <p>Explora las tecnologia que utilizo para crear expecionales experiencias digitales.</p>
-          </div>
-          <div>
-            {toolboxItems.map((item) => (
-              <div key={item.title} className="">
-                <TechIcon component={item.iconType} />
-                <span className="">{item.title}</span>
-              </div>
-            ))}
-          </div>
-        </Card>
-        <Card>
-          <div>
-            <StarIcon />
-            <h2>Detras del Codigo</h2>
-            <p>Explora mis intereses y hobbies detras del mundo digital.</p>
-          </div>
-        </Card>
-        <Card>
-          <Image src={mapImage} alt="Map Location" />
-          <Image src={smileMemoji} alt="Smiling emoji" />
-        </Card>
+    <section className="py-20">
+      <div className="container">
+        <SectionHeader
+          text="Sobre Mi"
+          title="Un Vistazo De Mi Mundo"
+          description="Aprende mas sobre quien soy, lo que me gusta hacer y lo que me inspira para mejorar."
+        />
+        <div className="mt-20">
+          <Card className="h-[320px]">
+            <CardHeader title="Mis Logros" description="Explora las certificaciones que avalan mis conocimientos." />
+            <div className="w-40 mx-auto mt-8">
+              <Image src={bookImage} alt="Book Cover" />
+            </div>
+          </Card>
+          <Card className="h-[320px]">
+            <CardHeader
+              title="Mis Herramientas"
+              description="Explora las tecnologia que utilizo para crear expecionales experiencias digitales."
+            />
+            <div>
+              {toolboxItems.map((item) => (
+                <div key={item.title} className="">
+                  <TechIcon component={item.iconType} />
+                  <span className="">{item.title}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card className="h-[320px]">
+            <CardHeader
+              title="Detras del Codigo"
+              description="Explora mis intereses y hobbies detras del mundo digital."
+            />
+            <div>
+              {hobbies.map((hobby) => (
+                <div key={hobby.title} className="flex gap-2">
+                  <span className="">{hobby.title}</span>
+                  <Image src={hobby.emoji} alt={hobby.title} className="size-6" />
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card className="h-[320px]">
+            <Image src={mapImage} alt="Map Location" />
+            <Image src={smileMemoji} alt="Smiling emoji" />
+          </Card>
+        </div>
       </div>
     </section>
   );
