9c2b9fb09362b95d01d23654212d7aa7a3debc42
Martin Maiolo <35736622+martinmaiolo@users.noreply.github.com>
2024-09-01 23:33:49 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
@@ -6,11 +6,75 @@ import { twMerge } from "tailwind-merge";
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 const calistoga = Calistoga({ subsets: ["latin"], variable: "--font-serif", weight: ["400"] });
 
-export const metadata: Metadata = {
-  title: "Martin Maiolo",
-  description: "Bienvenido a mi Web Portfolio, en donde encontraras toda la informacion que necesitas sobre mi!",
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
 };
 
+export const metadata: Metadata = {
+  metadataBase: new URL("https://service-electrolux.ar"),
+  title:
+    "▷ Service de Heladeras y Lavarropas ❄️ ELECTROLUX | Arreglos EN EL DÍA",
+  description:
+    "Service autorizado ELECTROLUX ✓ Ingresa y contactanos - Servicio Tecnico de heladeras y lavarropas ESPECIALIZADO y ¡Atendido por sus Dueños!",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+  openGraph: {
+    type: "website",
+    images:
+      "https://service-electrolux-f4204ruh5-fededinuzzos-projects.vercel.app/opengraph-image.jpg?405fc21523a0661c",
+    url: "https://service-electrolux.ar/",
+    title:
+      "▷ Service de Heladeras y Lavarropas ❄️ ELECTROLUX | Arreglos EN EL DÍA",
+    description:
+      "Service autorizado ELECTROLUX ✓ Ingresa y contactanos - Servicio Tecnico de heladeras y lavarropas ESPECIALIZADO y ¡Atendido por sus Dueños!",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "service-electrolux",
+    title:
+      '▷ Service de Heladeras y Lavarropas ❄️ ELECTROLUX | Arreglos EN EL DÍA"',
+    description:
+      "ervice autorizado ELECTROLUX ✓ Ingresa y contactanos - Servicio Tecnico de heladeras y lavarropas ESPECIALIZADO y ¡Atendido por sus Dueños!",
+    images:
+      "https://service-electrolux-f4204ruh5-fededinuzzos-projects.vercel.app/opengraph-image.jpg?405fc21523a0661c",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  keywords: [
+    "servicio",
+    "service",
+    "electrolux",
+    "heladeras",
+    "lavarropas",
+    "reparacion",
+    "arreglo",
+    "tecnicos",
+    "asistencia",
+    "capital federal",
+    "zona norte",
+    "zona sur",
+    "servicio tecnico de heladeras y lavarropas electrolux",
+    "service de lavarropas y heladeras electrolux",
+    "reparacion de heladeras y lavarropas electrlux",
+  ],
+  creator: "Federico Di Nuzzo",
+  generator: "Next.js",
+  publisher: "Vercel",
+  verification: {
+    google: "google",
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
